fd33a3e61223f4a3854033270fa8781a7ecbb387
Yatharth Mathur <yatharth@hackerearth.com>
2024-08-03 19:08:59 +0530

    fix: setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="django-neomodel",
-    version="5.4.0",
+    version="0.3.0",
     description="Custom Django Neomodel to support 5.22.0",
-    url="https://github.com/myplace/neomodel/commit/da18b6047d7d332a5d2bbf90b74dbdd0d911f73b",
+    url="https://github.com/myplace/django-neomodel",
     author="MyPlace",
     author_email="",
     license="BSD 2-clause",
